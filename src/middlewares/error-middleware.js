cf7e5c366c5bd92736cc3476fdf0e399b87cf5e1
rrtxt <rizfiferdian1@gmail.com>
2025-02-19 16:49:50 +0700

    refactor(middleware): edit response in error validation

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -2,7 +2,7 @@ import { ZodError } from "zod"
 
 const errorMiddleware = (error, request, response, nextFunction) => {
   console.error("ERR: ", error)
-  if (error instanceof ZodError) response.status(400).json({ message: error.message })
+  if (error instanceof ZodError) response.status(400).json({ message: error.errors })
   response.status(500).json({ message: error.message })
 }
 
