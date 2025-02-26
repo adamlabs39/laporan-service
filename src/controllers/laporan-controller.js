90d35bccbbda0578cd2fec04bfbf7761ec26fdda
rrtxt <rizfiferdian1@gmail.com>
2025-02-26 13:27:07 +0700

    feat(api): add get rekapitulasi lab in laporan controller

--- src/controllers/laporan-controller.js
+++ src/controllers/laporan-controller.js
@@ -6,8 +6,33 @@ export default class LaporanController {
       const result = await LaporanService.getRekapitulasiKunjungan(request.query)
 
       return response.status(200).json({
-        success: true,
-        message: 'Data rekapitulasi kunjungan berhasil didapatkan',
+        message: 'Data berhasil ditampilkan',
+        ...result
+      })
+    } catch (error) {
+      nextFunction(error)
+    }
+  }
+
+  static async getRekapitulasiTindakan(request, response, nextFunction) {
+    try {
+      const result = await LaporanService.getRekapitulasiTindakan(request.query)
+
+      return response.status(200).json({
+        message: 'Data berhasil ditampilkan',
+        ...result
+      })
+    } catch (error) {
+      nextFunction(error)
+    }
+  }
+
+  static async getRekapitulasiLab(request, response, nextFunction) {
+    try {
+      const result = await LaporanService.getRekapitulasiLab(request.query)
+
+      return response.status(200).json({
+        message: 'Data berhasil ditampilkan',
         ...result
       })
     } catch (error) {
