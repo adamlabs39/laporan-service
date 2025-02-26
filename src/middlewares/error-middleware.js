d97bb6f43f7aad404c2c4b892a73ec927ea7b348
rrtxt <rizfiferdian1@gmail.com>
2025-02-26 13:11:17 +0700

    feat(middleware): add bad request handling

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -1,8 +1,10 @@
 import { ZodError } from "zod"
+import BadRequestException from "../exceptions/bad-request-exception.js"
 
 const errorMiddleware = (error, request, response, nextFunction) => {
   console.error("ERR: ", error)
   if (error instanceof ZodError) response.status(400).json({ message: error.errors })
+  if (error instanceof BadRequestException) response.status(error.code).json({ message: error.message })
   response.status(500).json({ message: error.message })
 }
 
