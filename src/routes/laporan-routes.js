bdbefb8d781655f8ba5d6d07ca1f57028dcc63ba
rrtxt <rizfiferdian1@gmail.com>
2025-02-26 13:29:20 +0700

    feat(routes): add GET /rekap/lab endpoint

--- src/routes/laporan-routes.js
+++ src/routes/laporan-routes.js
@@ -4,5 +4,6 @@ import LaporanController from "../controllers/laporan-controller.js"
 const laporanRoutes = express.Router()
 laporanRoutes.get("/rekap/status-rawat", LaporanController.getRekapitulasiKunjungan)
 laporanRoutes.get("/rekap/tindakan", LaporanController.getRekapitulasiTindakan)
+laporanRoutes.get("/rekap/lab", LaporanController.getRekapitulasiLab)
 
 export default laporanRoutes
