8b0631851e677bb7b0d54f78b1ee00f26375b8ac
rrtxt <rizfiferdian1@gmail.com>
2025-03-07 09:00:12 +0700

    feat(api): add export rekapitulasi kunjungan, tindakan, and lab in laporan controller

--- src/controllers/laporan-controller.js
+++ src/controllers/laporan-controller.js
@@ -1,4 +1,5 @@
 import LaporanService from "../services/laporan-service.js";
+import fs from "fs"
 
 export default class LaporanController {
   static async getRekapitulasiKunjungan(request, response, nextFunction) {
@@ -35,6 +36,45 @@ export default class LaporanController {
         message: 'Data berhasil ditampilkan',
         ...result
       })
+    } catch (error) {
+      nextFunction(error)
+    }
+  }
+
+  static async exportRekapitulasiKunjungan(request, response, nextFunction) {
+    try {
+      const buffer = await LaporanService.exportRekapitulasiKunjungan(request.query)
+
+      response.setHeader('Content-Disposition', 'attachment; filename="rekapitulasi_kunjungan.xlsx"')
+      response.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      return response.status(200).send(buffer)
+    } catch (error) {
+      nextFunction(error)
+    }
+  }
+
+  static async exportRekapitulasiTindakan(request, response, nextFunction) {
+    try {
+      const buffer = await LaporanService.exportRekapitulasiTindakan(request.query)
+
+      response.setHeader('Content-Disposition', 'attachment; filename="rekapitulasi_tindakan.xlsx"')
+      response.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      return response.status(200).send(buffer)
+    } catch (error) {
+      nextFunction(error)
+    }
+  }
+
+  static async exportRekapitulasiLab(request, response, nextFunction) {
+    try {
+      const buffer = await LaporanService.exportRekapitulasiKunjungan(request.query)
+
+      response.setHeader('Content-Disposition', 'attachment; filename="rekapitulasi_pemeriksaan_lab.xlsx"')
+      response.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+      return response.status(200).send(buffer)
     } catch (error) {
       nextFunction(error)
     }
